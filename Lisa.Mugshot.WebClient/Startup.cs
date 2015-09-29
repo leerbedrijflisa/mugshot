21aa770bf7c5e31ebed359b71a1c394c6cdc0d9e
William Willing <william@williamwilling.com>
2015-09-29 10:17:12 +0200

    Created the web client project.

---
+++ Lisa.Mugshot.WebClient/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.Builder;
+
+namespace Lisa.Mugshot.WebClient
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            // Note that you must call UseDefaultFiles() before UseStaticFiles(), otherwise
+            // UseDefaultFiles() doesn't work.
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+        }
+    }
+}
